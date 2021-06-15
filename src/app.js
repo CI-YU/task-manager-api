b92bf2b5db34f7d9770722baa975c12ab6fa9e03
CI-YU <a1245bill@yahoo.com.tw>
2021-06-15 20:54:56 +0800

    perf: 將bcryptjs改為bcrypt提升效能

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const output = require('../swagger_output.json');
 
 const app = express();
 
-app.use(compression());
+// app.use(compression());
 app.use(helmet());
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(output));
