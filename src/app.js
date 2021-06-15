0bebaef39e808556e7a2085b9ff8925dd629b079
CI-YU <a1245bill@yahoo.com.tw>
2021-06-15 12:31:53 +0800

    add helmet & compression

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
 require('./db/mongoose.js');
 const userRouter = require('./router/user');
 const taskRouter = require('./router/task');
@@ -6,9 +8,11 @@ const swaggerUi = require('swagger-ui-express');
 const output = require('../swagger_output.json');
 
 const app = express();
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(output));
 
+app.use(compression());
+app.use(helmet());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(output));
 app.use('/users', userRouter);
 app.use('/tasks', taskRouter);
 
